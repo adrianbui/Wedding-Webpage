b93cc36933203b4253efaf3df82965480c42208d
Adrian Bui <adrian.t.bui@gmail.com>
2021-08-15 17:24:52 -0500

    fixed google map

--- js/scripts.js
+++ js/scripts.js
@@ -240,32 +240,32 @@ $(document).ready(function () {
 
 // Google map
 function initMap() {
-    var location = {lat: 22.5932759, lng: 88.27027720000001};
+    var creekside = {lat: 45.6902361, lng: -92.9358908};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
-        center: location,
+        center: creekside,
         scrollwheel: false
     });
 
     var marker = new google.maps.Marker({
-        position: location,
+        position: creekside,
         map: map
     });
 }
 
-function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
-    var map = new google.maps.Map(document.getElementById('map-canvas'), {
-        zoom: 15,
-        center: la_fiesta,
-        scrollwheel: false
-    });
+// function initBBSRMap() {
+//     var la_fiesta = {lat: 45.690236123106295, lng: -92.93589088911176};
+//     var map = new google.maps.Map(document.getElementById('map-canvas'), {
+//         zoom: 15,
+//         center: la_fiesta,
+//         scrollwheel: false
+//     });
 
-    var marker = new google.maps.Marker({
-        position: la_fiesta,
-        map: map
-    });
-}
+//     var marker = new google.maps.Marker({
+//         position: la_fiesta,
+//         map: map
+//     });
+// }
 
 // alert_markup
 function alert_markup(alert_type, msg) {
