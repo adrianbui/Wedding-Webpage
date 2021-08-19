51e88cfc9a4a02a25169cf59923e72711e4b86d4
Adrian Bui <adrian.t.bui@gmail.com>
2021-08-19 11:10:21 -0500

    added reception address to scripts.js file for calendar

--- js/scripts.js
+++ js/scripts.js
@@ -194,13 +194,13 @@ $(document).ready(function () {
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Jan 02, 2022 23:00'),
+            end: new Date('Jan 02, 2022 17:00'),
 
             // Event Address
             address: '8555 Rushseba Trail, Rush City, MN 55069, USA',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please email us at abbyadrian.wedding@gmail.com"
+            description: "The reception address is 1827 Riverside Ave, Minneapolis, MN 55454 (1 hour drive from the ceremony location). We can't wait to see you on our big day. For any queries or issues, please email us at abbyadrian.wedding@gmail.com"
         }
     });
 
